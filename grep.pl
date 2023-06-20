4ac9de0a61fc52f95372e6c28d9298e0aa181dcd
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-06-20 14:49:42 +0800

    Add files via upload

---
+++ grep.pl
@@ -0,0 +1,49 @@
+#! /usr/bin/perl -w
+use warnings;
+use strict;
+my $file = shift;
+my $output = shift;
+my $gff = shift;
+open I,"<$file" or die "Usage :: perl $0 vcfinput output gff";
+open O,">$output" or die "Usage :: perl $0 vcfinput output gff";
+open G,"<$gff" or die "Usage :: perl $0 vcfinput output gff";
+my %data;
+while(<G>)
+{
+	if(/\tgene\t/)
+	{
+	my @line = split(/\t/);
+	my ($chr,$source,$type,$start,$end,$score,$strand,$phase,$attributes) = @line;
+	$attributes =~ m/ID=(.*);Name=(.*);NameSource/;
+	my $id = $1;
+	my $name = $2;
+	$data{$id} = $name;
+	}
+
+}
+print STDERR "$gff loaded..\n";
+
+while(<I>)
+
+{
+	unless(/^#/){
+	my($chr,$pos,$id,$ref,$alt,$qual,$flt,$info,$format,$sample) = split(/\t/);
+	my @array =  split(/;/,$info);
+	if(/\bGenes=(\w+)/)
+	{
+	$array[1] =~ s/END=//g;
+	$info =~ m/Genes=(.*);?/;
+	my $id = $1;
+	if($data{$id} ne "" && $data{$id} ne "Unknown")
+		{
+		print O "$chr\t$pos\t$array[1]\t$array[0]\t$data{$id}\n";
+		}
+	else
+		{
+		print O "$chr\t$pos\t$array[1]\t$array[0]\t$id\n";
+		}
+}
+}
+}
+close I;
+close G;
